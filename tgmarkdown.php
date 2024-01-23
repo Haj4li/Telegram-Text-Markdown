d1347a79c0f3b740c2dfa3cb0be7f90a20549ce4
Ali Nakhaee <48994331+Haj4li@users.noreply.github.com>
2024-01-23 12:53:13 +0330

    Update tgmarkdown.php
    Fixed text and entities

--- tgmarkdown.php
+++ tgmarkdown.php
@@ -66,7 +66,7 @@ function MarkDown($string, $entities) {
     return $stringfinal;
 }
 
-$update = '{"update_id":0,"message":{"message_id":0,"from":{"id":0,"is_bot":false,"first_name":"0","username":"0","language_code":"0"},"chat":{"id":0,"first_name":"0","username":"0","type":"0"},"date":0,"text":"Its Me hello from earth  monotext strike  underline link \nBooooooSoooooldssssss","entities":[{"offset":0,"length":6,"type":"spoiler"},{"offset":13,"length":4,"type":"italic"},{"offset":18,"length":5,"type":"bold"},{"offset":25,"length":8,"type":"code"},{"offset":34,"length":6,"type":"strikethrough"},{"offset":42,"length":9,"type":"underline"},{"offset":52,"length":4,"type":"text_link","url":"http:\/\/google.com\/"},{"offset":57,"length":7,"type":"bold"},{"offset":64,"length":13,"type":"bold"},{"offset":64,"length":8,"type":"spoiler"}],"link_preview_options":{"is_disabled":true}}}';
+$update = '{"update_id":0,"message":{"message_id":0,"from":{"id":0,"is_bot":false,"first_name":"0","username":"0","language_code":"en"},"chat":{"id":0,"first_name":"0","username":"0","type":"private"},"date":0,"text":"Its Me hello from earth monotext strike underline link BooooooSoooooldssssss","entities":[{"offset":0,"length":6,"type":"spoiler"},{"offset":13,"length":4,"type":"italic"},{"offset":18,"length":5,"type":"bold"},{"offset":24,"length":8,"type":"code"},{"offset":33,"length":6,"type":"strikethrough"},{"offset":40,"length":9,"type":"underline"},{"offset":50,"length":4,"type":"text_link","url":"http:\/\/google.com\/"},{"offset":55,"length":7,"type":"bold"},{"offset":62,"length":8,"type":"bold"},{"offset":62,"length":8,"type":"spoiler"}],"link_preview_options":{"is_disabled":true}}}';
 $update = json_decode($update);
 
 echo MarkDown($update->message->text,$update->message->entities);
